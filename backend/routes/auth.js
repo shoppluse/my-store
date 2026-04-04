53fd9afe3bdeae0df1e66a40cd7c09f6288e7590
Anirudh <141948385+shoppluse@users.noreply.github.com>
2026-04-05 04:07:54 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,9 +7,7 @@ const router = express.Router();
 
 // Gmail SMTP transporter
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
