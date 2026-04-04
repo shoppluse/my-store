ce60d836e21d1c5a941c0758d81816d944680e9a
Anirudh <141948385+shoppluse@users.noreply.github.com>
2026-04-04 08:53:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const productsRouter = require("./routes/products");
+const authRouter = require("./routes/auth");
 
 dotenv.config();
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,8 +20,9 @@ app.get("/", (req, res) => {
   res.send("My Store Backend is running...");
 });
 
-// API routes
+// Routes
 app.use("/api/products", productsRouter);
+app.use("/api/auth", authRouter);
 
 // Start server
 app.listen(PORT, () => {
