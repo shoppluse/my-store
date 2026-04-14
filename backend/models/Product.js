1015d4874f77822057299d9842657c37f5b4cb23
Anirudh <141948385+shoppluse@users.noreply.github.com>
2026-04-14 23:30:18 +0530

    Update Product.js

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -8,70 +8,58 @@ const productSchema = new mongoose.Schema(
       required: true,
       trim: true
     },
-
     description: {
       type: String,
       default: "",
       trim: true
     },
-
     price: {
       type: mongoose.Schema.Types.Mixed,
       default: ""
     },
-
     image: {
       type: String,
       default: "",
       trim: true
     },
-
     images: {
       type: [String],
       default: []
     },
-
     category: {
       type: String,
       default: "Overall",
       trim: true
     },
-
     brand: {
       type: String,
       default: "",
       trim: true
     },
-
     link: {
       type: String,
       default: "",
       trim: true
     },
-
     rating: {
       type: Number,
       default: null
     },
-
     ownerId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true
     },
-
     ownerName: {
       type: String,
       default: "",
       trim: true
     },
-
     source: {
       type: String,
       default: "affiliate",
       trim: true
     },
-
     isActive: {
       type: Boolean,
       default: true
